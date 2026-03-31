a948b7a7034f388b0d780f204b697b8a3cd98878
quarterstar <quarterstar@proton.me>
2026-03-31 00:00:00 +0000

    fix: resolve c++ reserved naming convention foo__ violation

--- contourcpp.hpp
+++ contourcpp.hpp
@@ -37,13 +37,13 @@
 #include <variant>
 
 template <typename T>
-struct __is_expected : std::false_type {};
+struct _is_expected : std::false_type {};
 
 template <typename E>
-struct __is_expected<std::unexpected<E>> : std::true_type {};
+struct _is_expected<std::unexpected<E>> : std::true_type {};
 
 template <typename T, typename E>
-struct __is_expected<std::expected<T, E>> : std::true_type {};
+struct _is_expected<std::expected<T, E>> : std::true_type {};
 
 // `maybe` macros:
 //
@@ -58,11 +58,11 @@ struct __is_expected<std::expected<T, E>> : std::true_type {};
 // * Second argument (optional): the expression that yields a modified bad value
 //
 // NOTE: Within the second argument, you can use the bad value (if it exists) by referring to
-// `__maybe_error`
+// `_maybe_error`
 
 /// @brief Internal helper to extract error data from optionals or expecteds.
 template <typename T>
-auto __get_error(T&& container) -> decltype(auto) {
+auto _get_error(T&& container) -> decltype(auto) {
   if constexpr (requires { container.error(); }) {
     return std::forward<T>(container).error();
   } else {
@@ -71,7 +71,7 @@ auto __get_error(T&& container) -> decltype(auto) {
 }
 
 template <typename F, typename T>
-auto __get_result(F&& fallback, T&& error) -> decltype(auto) {
+auto _get_result(F&& fallback, T&& error) -> decltype(auto) {
   if constexpr (std::invocable<F, T>) {
     return std::forward<F>(fallback)(std::forward<T>(error));
   } else {
@@ -80,7 +80,7 @@ auto __get_result(F&& fallback, T&& error) -> decltype(auto) {
 }
 
 template <typename T>
-auto __deref_or_void(T&& container) -> decltype(auto) {
+auto _deref_or_void(T&& container) -> decltype(auto) {
   using value_type = typename std::decay_t<T>::value_type;
   if constexpr (std::is_void_v<value_type>) {
     return;
@@ -90,15 +90,15 @@ auto __deref_or_void(T&& container) -> decltype(auto) {
 }
 
 template <typename T>
-concept __contextual_bool = requires(T&& v) { static_cast<bool>(std::forward<T>(v)); };
+concept _contextual_bool = requires(T&& v) { static_cast<bool>(std::forward<T>(v)); };
 
 template <typename T>
-concept __referencable = std::is_reference_v<std::add_lvalue_reference_t<T>>;
+concept _referencable = std::is_reference_v<std::add_lvalue_reference_t<T>>;
 
 template <typename Storage>
-struct __maybe_failure_proxy {
+struct _maybe_failure_proxy {
 private:
-  using Self = __maybe_failure_proxy;
+  using Self = _maybe_failure_proxy;
 
 public:
   Storage value;
@@ -118,7 +118,7 @@ public:
         "E must be default-initializable to convert from optional failure."
       );
       return std::unexpected(E {});
-    } else if constexpr (__is_expected<value_type>::value) {
+    } else if constexpr (_is_expected<value_type>::value) {
       return std::forward<value_type>(self.value);
     } else {
       return std::unexpected<E>(std::forward<value_type>(self.value));
@@ -127,7 +127,7 @@ public:
 };
 
 template <typename T>
-__maybe_failure_proxy(T&&) -> __maybe_failure_proxy<std::decay_t<T>>;
+_maybe_failure_proxy(T&&) -> _maybe_failure_proxy<std::decay_t<T>>;
 
 /// @brief Macro router to support different argument counts.
 #define GET_maybe_MACRO(_1, _2, NAME, ...) NAME
@@ -139,11 +139,11 @@ __maybe_failure_proxy(T&&) -> __maybe_failure_proxy<std::decay_t<T>>;
 /// Example: maybe(self.update());
 #define maybe_1(expr)                                                                              \
   ({                                                                                               \
-    auto&& __result {(expr)};                                                                      \
-    if (!__result) {                                                                               \
-      return ::__maybe_failure_proxy {::__get_error(__result)};                                    \
+    auto&& _result {(expr)};                                                                       \
+    if (!_result) {                                                                                \
+      return ::_maybe_failure_proxy {::_get_error(_result)};                                       \
     }                                                                                              \
-    ::__deref_or_void(std::move(__result));                                                        \
+    ::_deref_or_void(std::move(_result));                                                          \
   })
 
 /// @brief Case 2: maybe(expression, fallback_expression)
@@ -151,17 +151,17 @@ __maybe_failure_proxy(T&&) -> __maybe_failure_proxy<std::decay_t<T>>;
 /// Evaluates to the inner value of the expected/optional.
 /// If an error occurs, returns the fallback_expression from the parent function.
 ///
-/// NOTE: `__e` is the error and available within the fallback_expression.
+/// NOTE: `_e` is the error and available within the fallback_expression.
 ///
-/// Example: auto val = maybe(get_val(), __e);
+/// Example: auto val = maybe(get_val(), _e);
 #define maybe_2(expr, fallback)                                                                    \
   ({                                                                                               \
-    auto&& __result {(expr)};                                                                      \
-    if (!__result) {                                                                               \
-      [[maybe_unused]] auto&& __e {::__get_error(__result)};                                       \
-      return ::__maybe_failure_proxy {__get_result(fallback, __e)};                                \
+    auto&& _result {(expr)};                                                                       \
+    if (!_result) {                                                                                \
+      [[maybe_unused]] auto&& _e {::_get_error(_result)};                                          \
+      return ::_maybe_failure_proxy {_get_result(fallback, _e)};                                   \
     }                                                                                              \
-    ::__deref_or_void(std::move(__result));                                                        \
+    ::_deref_or_void(std::move(_result));                                                          \
   })
 
 // Helpers
